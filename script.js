c0732f45aa7ce70e5f8907342b488cf2ed7bc948
drossano <72749793+drossano@users.noreply.github.com>
2021-09-26 16:13:20 -0400

    Make grid have multiple rows

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ for (let i = 0; i < gridDimension; i++){
     const gridColumn = document.createElement('div');
     gridColumn.classList.add('grid-column-' + i);
     container.appendChild(gridColumn);
-    gridColumn.setAttribute('style', 'border:2px solid black; height: 100%; width: 100%;');
+    gridColumn.setAttribute('style', 'display:flex; flex-direction: column; height: 100%; width: 100%;');
+    for (let j= 0; j < gridDimension; j++){
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square-'+j);
+        gridColumn.appendChild(gridSquare);
+        gridSquare.setAttribute('style', "height: 100%; width 100%; border-collapse: collapse; border: 1px solid black;")
+    }
 }
 
 
