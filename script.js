ce1e630c306698141359097d52807d6ddfe3af2f
drossano <72749793+drossano@users.noreply.github.com>
2021-09-27 17:26:17 -0400

    Create drawGrid function

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const container = document.querySelector('#grid-container');
 
-const gridDimension = 16
 
-for (let i = 0; i < gridDimension; i++){
-    const gridColumn = document.createElement('div');
-    gridColumn.classList.add('grid-column-' + i);
-    container.appendChild(gridColumn);
-    gridColumn.setAttribute('style', 'display:flex; flex-direction: column; height: 100%; width: 100%;');
-    for (let j= 0; j < gridDimension; j++){
-        const gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid-square-'+j);
-        gridColumn.appendChild(gridSquare);
-        gridSquare.setAttribute('style', "height: 100%; width 100%; border-collapse: collapse; border: 1px solid black;")
+
+
+const button = document.querySelector("button");
+button.addEventListener('click', () =>{
+    gridDimension = prompt ();
+    
+})
+
+function drawGrid(gridDimension) {
+    for (let i = 0; i < gridDimension; i++){
+        const gridColumn = document.createElement('div');
+        gridColumn.classList.add('grid-column-' + i);
+        container.appendChild(gridColumn);
+        gridColumn.setAttribute('style', 'display:flex; flex-direction: column; height: 100%; width: 100%;');
+        for (let j= 0; j < gridDimension; j++){
+            const gridSquare = document.createElement('div');
+            gridSquare.classList.add('grid-square-'+j);
+            gridColumn.appendChild(gridSquare);
+            gridSquare.setAttribute('style', "height: 100%; width 100%; border-collapse: collapse; border: 1px solid black;")
+        }
     }
+    return;
 }
 
+drawGrid(16);
 
 const gridSquare = document.querySelectorAll('[class^="grid-square-"]')
 
